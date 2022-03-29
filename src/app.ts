9c7539c8a22ee6b4d0f9a41d0f99615e636162ea
Xafixav <xafiworld@gmail.com>
2022-03-29 18:04:27 -0300

    :bento: add '/products' routes and error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import productRoutes from './routes/productsRoutes';
+import Error from './middleware/error';
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', productRoutes);
+app.use(Error.consoleError);
 
 export default app;
