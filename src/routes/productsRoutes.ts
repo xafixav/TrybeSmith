8a10d1b983e4bd6a95de585e13a4796179f0a677
Xafixav <xafiworld@gmail.com>
2022-03-29 21:43:46 -0300

    :white_check_mark: add route Post endpoint

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
 import ProductsController from '../controller/products';
+import ValidationProduct from '../middleware/validationProduct';
 
 const router = Router();
 
 const productsController = new ProductsController();
+const middleware = new ValidationProduct();
 
 router
   .route('/')
   .get(async (req, res, next) =>
     productsController.getAll(req, res, next));
+
+router
+  .route('/')
+  .post(
+    async (req, res, next) =>
+      middleware.validatePost(req, res, next),
+    async (req, res, next) =>
+      productsController.create(req, res, next),
+  );
 
 export default router;
