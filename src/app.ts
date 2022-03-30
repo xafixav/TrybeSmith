e2a6180707e1999343308e6fc4d3545e9e00e67f
Xafixav <xafiworld@gmail.com>
2022-03-30 18:02:21 -0300

    :white_check_mark: add order Routes into app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-import routerProducts from './routes/productsRoutes';
 import ErrorMiddleware from './middleware/error';
+import routerProducts from './routes/productsRoutes';
 import routerUser from './routes/userRoutes';
+import routerOrders from './routes/orderRoutes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', routerProducts);
 app.use('/users', routerUser);
+app.use('/orders', routerOrders);
 app.use(ErrorMiddleware.consoleError);
 
 export default app;
