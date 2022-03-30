765bd23fceda751b102028ef3c579d18aad05cdd
Xafixav <xafiworld@gmail.com>
2022-03-29 22:48:20 -0300

    :bento: add users routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import productRoutes from './routes/productsRoutes';
+import routerProducts from './routes/productsRoutes';
 import ErrorMiddleware from './middleware/error';
+import routerUser from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
-app.use('/products', productRoutes);
+app.use('/products', routerProducts);
+app.use('/users', routerUser);
 app.use(ErrorMiddleware.consoleError);
 
 export default app;
