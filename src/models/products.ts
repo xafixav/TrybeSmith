59e2d38d1baf40815c906d3ed5be78dbfd2bd567
Xafixav <xafiworld@gmail.com>
2022-03-29 21:41:58 -0300

    :bento: add create into product model

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,4 +1,4 @@
-import { Pool, RowDataPacket } from 'mysql2/promise';
+import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 import IProduct from '../interface/IProduct';
 
 export default class ProductModel {
@@ -9,5 +9,13 @@ export default class ProductModel {
     SELECT * FROM Trybesmith.Products
     `);
     return result as IProduct[];
+  }
+
+  public async createProduct(data: IProduct): Promise<IProduct> {
+    const { name, amount } = data;
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(`
+    INSERT into Trybesmith.Products(name, amount)
+    VALUES (?, ?)`, [name, amount]);
+    return { id: insertId, name, amount };
   }
 }
